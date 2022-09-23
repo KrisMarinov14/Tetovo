7f1b67a6f56f398663851190e869d03cdb8b7838
KrisMarinov14 <112855275+KrisMarinov14@users.noreply.github.com>
2022-09-23 17:55:52 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<title>Index</title>
+<?php
+include('https://development.forma.bg/vip/LibraryTetovo/components/head.php');
+include('https://development.forma.bg/vip/LibraryTetovo/components/header.php');
+include('https://development.forma.bg/vip/LibraryTetovo/components/carousel-index.php');
+include('https://development.forma.bg/vip/LibraryTetovo/components/text-image.php');
+?>
+
+<img src="https://development.forma.bg/vip/LibraryTetovo/images/shadow-up.png" id="shadow1" class="w-100">
+
+<?php
+include('https://development.forma.bg/vip/LibraryTetovo/components/2news.php');
+include('https://development.forma.bg/vip/LibraryTetovo/components/video.php');
+include('https://development.forma.bg/vip/LibraryTetovo/components/contactForm.php');
+include('https://development.forma.bg/vip/LibraryTetovo/components/footer.php');
+?>
+
+
+    <div class="vip mb-0">
+        <p class="mb-0">
+            ВИП иновейшънс &copy; 2021-2022 &#124; Design & Development by FORMA Design & Innovations
+        </p>
+    </div>
